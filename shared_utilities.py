2e8879764d124a937602449ee27aaf21b90e139e
Vanessa Bennett <vbennett2@wisc.edu>
2026-08-06 21:03:56 -0500

    Revert "bug fix"
    This reverts commit 82c2c409c7c827e3421dc8b9f697f5c595dfba64.

--- shared_utilities.py
+++ shared_utilities.py
@@ -56,7 +56,7 @@ def db_write(listing_data):
                 cursor.execute("""
                 SELECT COALESCE(MAX(days_vacant), 0) 
                 FROM listings 
-                WHERE date_logged = %s AND identifier = %s AND building_id = %s;""", (yesterday, item[1], building_id))
+                WHERE date_logged = %s AND identifier = %s;""", (yesterday, item[1]))
     
                 days_vacant = (cursor.fetchone()[0])+1;
                 total_days_vacant +=days_vacant
