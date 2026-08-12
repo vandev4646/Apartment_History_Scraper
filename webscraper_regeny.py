231953b40ad7ef828e842fb12ec78d58104d4a1e
Vanessa Bennett <vbennett2@wisc.edu>
2026-08-11 21:25:47 -0500

    debugging

--- webscraper_regeny.py
+++ webscraper_regeny.py
@@ -63,7 +63,7 @@ def apartment_data(apartment: Apartment):
     #get all the url's on the page and select the ones assoicated with details
     urls = [tag['href'] for tag in soup.find_all(class_="more_detail_btn")]
     print("printing Urls")
-    print(url)
+    print(urls)
     listing_data = []
     
     #get the data for each listing
